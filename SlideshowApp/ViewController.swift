213195ad79d893837a51f3627d1e3af8a0b4db15
yoshiki-nakamura2 <n.yoshiki5141223@icloud.com>
2020-06-10 21:02:10 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 中村嘉希 on 2020/06/10.
+//  Copyright © 2020 yoshiki.nakamura2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
